186bd01c1eb44f3cce015e7d77a32dd4375b6a82
Michael Atere <michaelatere@yahoo.com>
2022-07-03 10:56:27 +0100

    Created my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	char myStr[] = "\"Programming is like building a multilingual puzzle\n";
 
+	puts(myStr);
 	return (0);
 }
