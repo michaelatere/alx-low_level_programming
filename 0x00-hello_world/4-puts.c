3fcefbd18cac50d68a6d6dd209d7c5ad467f7b5a
Michael Atere <michaelatere@yahoo.com>
2022-07-03 10:33:35 +0100

    Created my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 /**
  *  main - Entry point
- *  Description: 'This program prints some charater to the stdout using the put() function'
+ *  Description: 'This program prints some charater using the put()'
  *  Return: 0 (Success)
  */
-
 int main(void)
 {
-	char myStr[] ="Programming is like building a multulingual puzzle\n";
+	char myStr[] = "Programming is like building a multilingual puzzle\n";
+
 	puts(myStr);
-	return 0;
+
+	return (0);
 }
