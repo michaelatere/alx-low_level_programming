a6bc2f0cb241e45a01f8f63fac34cebfff0c09d5
Michael Atere <michaelatere@yahoo.com>
2022-07-04 06:02:57 +0100

    first error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Prints a stderr'
+ * Return: 1 (error)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	return (1);
+}
