ee02b72219f602993fd5c5bf47c799dc59d5d585
Michael Atere <michaelatere@yahoo.com>
2022-07-04 06:19:47 +0100

    first error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 50);
+	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
